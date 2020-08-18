b31743743b40e3e56779593bf3a58363a016499c
kaistemimi <temimikais@gmail.com>
2020-08-18 12:38:36 +0100

    project done i add some function in jquery and modify some css & html

--- pro.js
+++ pro.js
@@ -1,8 +1,26 @@
-var person = function(n) {
-    if(n<8){
-        return "you are Welcome";
-    }
-        return "choice other place";
-}
+$(document).ready(function(){
+	var kaisPage= '<h1 class="RA"> </h1><img id="imgg"src="" width="1300">'
 
-document.getElementById("demo").innerHTML=person;
+$("#res").click(function(){	
+
+	var n=$("#numPer").val()
+	console.log( typeof n)
+    if(parseInt(n)<8){
+    	console.log("fff")
+    	$("body").html("")
+    	$(kaisPage).appendTo($("body"))
+        $(".RA").append("Reservation Approved")
+        $("#imgg").attr("src","validee.jpg")
+    }else{
+        $("body").html("")
+    	$(kaisPage).appendTo($("body"))
+        $(".RA").append("No Place!! Choose Another")
+         $("#imgg").attr("src","croi rouge.jpg")
+    }
+
+})
+
+})
+
+
+
